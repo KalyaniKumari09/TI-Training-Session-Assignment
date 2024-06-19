b15167d168eee298659c72f517395ea7ddc3ff55
KalyaniKumari09 <113120303+KalyaniKumari09@users.noreply.github.com>
2024-06-19 15:35:17 +0530

    Count.js

--- Count.js
+++ Count.js
@@ -1,1 +1,38 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increments = [1, 5, 10, 15, 20];
+
+  const increment = (value) => {
+    setCount(count + value);
+  };
+
+  const decrement = (value) => {
+    setCount(count - value);
+  };
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <div>
+        {increments.map((inc) => (
+          <button key={inc} onClick={() => increment(inc)}>
+            +{inc}
+          </button>
+        ))}
+      </div>
+      <div>
+        {increments.map((dec) => (
+          <button key={dec} onClick={() => decrement(dec)}>
+            -{dec}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
 
